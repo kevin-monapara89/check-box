6da5779301e2cafade20b9b07fd9f1e9eab9b6d6
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-02-22 10:35:57 -0500

    commit

--- app/src/main/java/com/kevin/checkbox/MainActivity.java
+++ app/src/main/java/com/kevin/checkbox/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kevin.checkbox;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     CheckBox cricket, football, hockey, kabaddi, chess, running, carrace;
     Button submit;
-    TextView result
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Binding();
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,22 +32,35 @@ public class MainActivity extends AppCompatActivity {
                     s += "Football \n";
                 }
                 if (hockey.isChecked()) {
-                    s += "Cricket \n";
+                    s += "Hockey \n";
                 }
                 if (kabaddi.isChecked()) {
-                    s += "Cricket \n";
+                    s += "Kabaddi \n";
                 }
                 if (chess.isChecked()) {
-                    s += "Cricket \n";
+                    s += "Chess \n";
                 }
                 if (running.isChecked()) {
-                    s += "Cricket \n";
+                    s += "Running \n";
                 }
                 if (carrace.isChecked()) {
-                    s += "Cricket \n";
+                    s += "Car raceing \n";
                 }
-
+                result.setText("Sports : "+s);
             }
         });
     }
+
+    private void Binding() {
+        cricket = findViewById(R.id.cricket);
+        football = findViewById(R.id.football);
+        hockey = findViewById(R.id.hockey);
+        kabaddi = findViewById(R.id.kabaddi);
+        chess = findViewById(R.id.chess);
+        running = findViewById(R.id.running);
+        carrace = findViewById(R.id.carrace);
+        submit = findViewById(R.id.submit);
+        result = findViewById(R.id.result);
+    }
+    
 }
